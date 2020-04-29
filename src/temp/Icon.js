71c47aef1ee3bc1c92d62e111673ec54953c9f0b
Mohit Singh <mohitsingh.shiv@gmail.com>
2020-04-29 15:41:13 +0530

    icon data

--- src/temp/Icon.js
+++ src/temp/Icon.js
@@ -1,7 +1,7 @@
 const imgData = [
   { id: 1, imgName: "chrome", link: "/index" },
-  { id: 1, imgName: "confluence", link: "/" },
-  { id: 1, imgName: "jira", link: "/" },
+  { id: 2, imgName: "confluence", link: "/" },
+  { id: 3, imgName: "jira", link: "/" },
 ];
 
 export default imgData;
